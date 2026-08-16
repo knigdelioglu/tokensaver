65ffc586e4731a3979836a474ab59bfe558ee992
Kadir niğdelioğlu <194108137+knigdelioglu@users.noreply.github.com>
2026-08-16 18:31:49 +0300

    Add CLI entry contract test source

---
+++ tests/cli_entry_contract.rs
@@ -0,0 +1,16 @@
+#[test]
+fn no_arguments_remain_desktop_mode() {
+    assert!(!tokensaver::should_run_cli(&[]));
+}
+
+#[test]
+fn normal_cli_command_selects_cli_mode() {
+    assert!(tokensaver::should_run_cli(&["status".to_owned()]));
+}
+
+#[test]
+fn macos_process_serial_number_argument_does_not_select_cli_mode() {
+    assert!(!tokensaver::should_run_cli(&[
+        "-psn_0_12345".to_owned()
+    ]));
+}
